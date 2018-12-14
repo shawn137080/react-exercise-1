7d5789219441fc63aa49c1cce02f41cbe94fd7a5
shawn137080 <shawn137080@gmail.com>
2018-12-15 00:58:44 +0800

    merged

--- src/components/dashboard/Noti.js
+++ src/components/dashboard/Noti.js
@@ -5,9 +5,15 @@ const Noticification = (props) => {
     <div className="section">
       <div className="card z-depth-0">
         <div className="card-content">
+          <span className="card-title">Noticification</span>
           <ul className="notifications">
             <li>Noticification</li>
             <li>Noticification</li>
           </ul>
         </div>
       </div>
+    </div>
+  );
+};
+
+export default Noticification;
