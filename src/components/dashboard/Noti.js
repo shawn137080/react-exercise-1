45bc97ae4400bdfd5081b82ee86609f5f42165c7
shawn137080 <shawn137080@gmail.com>
2018-12-15 00:57:20 +0800

    merged

--- src/components/dashboard/Noti.js
+++ src/components/dashboard/Noti.js
@@ -5,24 +5,9 @@ const Noticification = (props) => {
     <div className="section">
       <div className="card z-depth-0">
         <div className="card-content">
-<<<<<<< HEAD
-          <span className="card-title">Noticifications</span>
-          <ul className='noticifications'>
-            <li>Noticifications</li>
-            <li>Noticifications</li>
-            <li>Noticifications</li>
-            <li>Noticifications</li>
-=======
-          <span className="card-title">Noticification</span>
           <ul className="notifications">
             <li>Noticification</li>
             <li>Noticification</li>
->>>>>>> 14818278e484a31f56790d0f13e96f19a506f11d
           </ul>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Noticification;
